97c5a855bc2639a987b07bfc54b17adda27d0d6e
ravikishor27 <iam.ravikishor27@gmail.com>
2024-11-27 13:21:21 +0530

    Created Middleware to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.models.js";
 import bcryptjs from 'bcryptjs';
 
-export const signup = async(req,res)=>{
+export const signup = async(req,res,next)=>{
     const {username,email,password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({username, email, password:hashedPassword}); // to save in a data base we created a new User
@@ -9,6 +9,7 @@ export const signup = async(req,res)=>{
         await newUser.save()  //await is key to make file save with any error interuption, it keep on one process unitl it save
         res.status(201).json("User Created Successfully")
     } catch(error){
-        res.status(500).json(error.message)  // to catch error and to inform in insomania ui
+        //res.status(500).json(error.message)  // to catch error and to inform in insomania ui
+        next(error);
     }
 };
