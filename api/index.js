65f86274a870393c9dea23a187e5b7c59afb94dd
ravikishor27 <iam.ravikishor27@gmail.com>
2024-12-02 20:58:37 +0530

    Createuser API update api  route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_CONNECTION_ID).then(()=>{
@@ -14,6 +15,8 @@ mongoose.connect(process.env.MONGO_CONNECTION_ID).then(()=>{
 const app= express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000,()=>{
     console.log("Server is Successfully running in port 3000")
